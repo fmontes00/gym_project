5d3caa65e8ba3927115652640c856dd5e1782e76
fernando <fernandomontesdeoca93@gmail.com>
2022-07-03 02:24:32 -0300

    change to simple class Views

--- gym_project/gym/views.py
+++ gym_project/gym/views.py
@@ -1,4 +1,4 @@
-from multiprocessing import context
+from django.views import View
 from django.shortcuts import redirect, render
 from .models import Equipment
 from .forms import EquipmentForm, ExerciseForm
@@ -13,23 +13,43 @@ from django.views.generic.edit import FormView
 #     return render(request, "gym/home.html", {"equipments": equipments})
 
 
-class HomeView(TemplateView):
-    template_name = "gym/home.html"
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["equipments"] = Equipment.objects.all()
-        return context
+class HomeView(View):
+    def get(self, request):
+        equipments = Equipment.objects.all()
+        return render(request, "gym/home.html",{"equipments": equipments})
 
 
-class ExerciseFormView(FormView):
-    template_name = "gym/create_exercise.html"
+# class HomeView(TemplateView):
+#     template_name = "gym/home.html"
+
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context["equipments"] = Equipment.objects.all()
+#         return context
+
+
+# class ExerciseFormView(FormView):
+#     template_name = "gym/create_exercise.html"
+#     form_class = ExerciseForm
+#     success_url = "/gym"
+
+#     def form_valid(self, form):
+#         form.save()
+#         return super().form_valid(form)
+
+class ExerciseFormView(View):
+
     form_class = ExerciseForm
-    success_url = "/gym"
 
-    def form_valid(self, form):
+    def get(self, request):
+        form = self.form_class
+        return render(request, "gym/create_exercise.html", {"form": form})
+    
+    def post(self, request):
+        form = self.form_class(request.POST)               #ExerciseForm(request.POST)
         form.save()
-        return super().form_valid(form)
+        return redirect("home")
+
 
 
 # def create_exercise(request):
@@ -42,14 +62,28 @@ class ExerciseFormView(FormView):
 #         return redirect("home")
 
 
-class EquipmentFormView(FormView):
-    template_name = "gym/create_equipment.html"
-    form_class = EquipmentForm
-    success_url = "/gym"
+# class EquipmentFormView(FormView):
+#     template_name = "gym/create_equipment.html"
+#     form_class = EquipmentForm
+#     success_url = "/gym"
 
-    def form_valid(self, form):
+#     def form_valid(self, form):
+#         form.save()
+#         return super().form_valid(form)
+
+
+class EquipmentFormView(View):
+
+    form_class = EquipmentForm
+
+    def get(self,request):
+        form = self.form_class
+        return render(request,"gym/create_equipment.html", {"form": form})
+    
+    def post(self, request):
+        form = self.form_class(request.POST)
         form.save()
-        return super().form_valid(form)
+        return redirect("home")
 
 
 # def create_equipment(request):
