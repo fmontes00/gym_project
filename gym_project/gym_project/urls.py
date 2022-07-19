a8b6a15e505bce6a0498a3f95e37826661023259
fernando <fernandomontesdeoca93@gmail.com>
2022-07-18 23:59:03 -0300

    urls for main project

--- gym_project/gym_project/urls.py
+++ gym_project/gym_project/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("gym/", include("gym.urls")),
-    #path("accounts/",include("accounts.urls"))
+    path("accounts/",include("accounts.urls"))
 
 ]
