d53539e92497ebd476df05fa0a5674f7458eaea5
fernando <fernandomontesdeoca93@gmail.com>
2022-07-22 15:11:59 -0300

    users changes

--- gym_project/accounts/urls.py
+++ gym_project/accounts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
-from .views import SignUpView , HomeView
+from .views import SignUpView , HomeView, LoginView2
 
 urlpatterns = [
     path("signup/", SignUpView.as_view(), name="signup"),
     path("home/",HomeView.as_view(), name="home"),
+    path("login/",LoginView2.as_view(),name = "login"),
 ]
--- gym_project/accounts/views.py
+++ gym_project/accounts/views.py
@@ -1,16 +1,18 @@
-from django.shortcuts import render
-from . import urls
-
+from django.shortcuts import redirect, render
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.views import LoginView   
 # Create your views here.
 from django.urls import reverse_lazy
 from django.views.generic import CreateView , TemplateView
 from .forms import CustomUserCreationForm
+from django.contrib.auth.decorators import login_required
 
 
 
 class SignUpView(CreateView):
     form_class = CustomUserCreationForm
-    success_url = reverse_lazy('home')
+    success_url = reverse_lazy('login')
     template_name = "accounts/signup.html"
 
 
@@ -18,3 +20,7 @@ class SignUpView(CreateView):
 class HomeView(TemplateView):
     def get(self, request):
         return render(request, "accounts/home.html")
+
+
+class LoginView2(LoginView):
+    template_name = "accounts/login.html"
