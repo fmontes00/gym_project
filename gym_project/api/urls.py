edb972db0e5e285407edc5f89ba47b74198bf716
fernando <fernandomontesdeoca93@gmail.com>
2022-11-16 23:14:20 -0300

    equipments class base views and urls

--- gym_project/api/urls.py
+++ gym_project/api/urls.py
@@ -1,24 +1,19 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import (
-    EquipmentApiView,
-    ExerciseApiView,
-    equipment_list,
-    equipment_retrieve,
-    equipment_edition,
-    create_equipment,
-    delete_equipment,
     ExerciseViewSet,
+    EquipmentViewSet,
 )
 
 
 router = DefaultRouter(trailing_slash=False)
 router.register(r"exercises", ExerciseViewSet, basename="exercise")
+router.register(r"equipments",EquipmentViewSet, basename="equipment")
 
 urlpatterns = [
     # path("equipments/",EquipmentApiView.as_view(), name="equipment_list"),
     # path("exercises", ExerciseViewSet, name="exercises"),
-    path("equi/", equipment_list, name="equi"),
+    #path("equi/", equipment_list, name="equi"),
     path("", include(router.urls))
     # path("eq/<int:pk>", equipment_retrieve, name="eq"),
     # path("edit/<int:pk>", equipment_edition, name="edit"),
--- gym_project/api/views.py
+++ gym_project/api/views.py
@@ -55,74 +55,154 @@ class ExerciseViewSet(viewsets.ViewSet):
 
 
 
-class EquipmentApiView(generics.ListAPIView):
-    queryset = Equipment.objects.all()
-    serializer_class = EquipmentSerializer
 
-
-class ExerciseApiView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Exercise.objects.all()
-    serializer_class = ExerciseSerializer
-
-
-@api_view()
-def equipment_list(request):
-    # agarrar todos los equipments
-    equipments = Equipment.objects.all()
-    # armar lista de equipments
-
-    serializer = EquipmentSerializer(equipments, many=True).data
-    # deveolver lista de equipments
-
-    return Response(serializer)
-
-
-@api_view()
-def equipment_retrieve(request, pk):
-    ipdb.set_trace()
-    import ipdb
-
-    ipdb.set_trace()
-    try:
-        equipment = Equipment.objects.get(id=pk)
-    except Equipment.DoesNotExist:
-        return Response({"detail": "Equipment not found"}, status=404)
-    # data = {
-    #     "name": equipment.name,
-    #     "description": equipment.description,
-    # }
-    serializer = EquipmentSerializer(equipment)
-
-    return Response(serializer.data)
-
-
-@api_view(["POST"])
-def create_equipment(request):
-
-    serializer = EquipmentSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
+class EquipmentViewSet(viewsets.ViewSet):
+    def list(self, request):
+        equipment = Equipment.objects.all()
+        serializer = EquipmentSerializer(equipment, many=True)
         return Response(serializer.data)
 
 
-@api_view(["PUT"])
-def equipment_edition(request, pk):
-    try:
-        equipment = Equipment.objects.get(id=pk)
-    except Equipment.DoesNotExist:
-        return Response({"detail": "Equipment not found"}, status=404)
-    serializer = EquipmentSerializer(equipment, data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def retrieve(self, request, pk= None):
+        try:
+            equipment = Equipment.objects.get(id=pk)
+        except Equipment.DoesNotExist:
+            return Response({"detail":"Equipment not found"}, status=status.HTTP_404_NOT_FOUND)
+        serializer = EquipmentSerializer(equipment)
+        return Response(serializer.data)
 
 
-@api_view(["DELETE"])
-def delete_equipment(request, pk):
-    equipment = Equipment.objects.get(id=pk)
-    equipment.delete()
-    return Response(status=status.HTTP_204_NO_CONTENT)
+    def create(self, request):
+        serializer = EquipmentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(
+            {"detail": "Given payload is invalid"}, status=status.HTTP_400_BAD_REQUEST
+        )
+        
+
+    def update(self, request, pk= None):
+        try:
+            equipment = Equipment.objects.get(id=pk)
+        except Equipment.DoesNotExist:
+            return Response({"detail": "Equipment not found"}, status=status.HTTP_404_NOT_FOUND)
+        serializer = EquipmentSerializer(equipment, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def partial_update():
+        pass
+
+    def destroy(self, request, pk=None):
+        try:
+            equipment = Equipment.objects.get(id=pk)
+        except Equipment.DoesNotExist:
+            return Response({"detail":"Equipment not found"}, status=status.HTTP_404_NOT_FOUND)
+        equipment.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class EquipmentApiView(generics.ListAPIView):
+#     queryset = Equipment.objects.all()
+#     serializer_class = EquipmentSerializer
+
+
+# class ExerciseApiView(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Exercise.objects.all()
+#     serializer_class = ExerciseSerializer
+
+
+# @api_view()
+# def equipment_list(request):
+#     # agarrar todos los equipments
+#     equipments = Equipment.objects.all()
+#     # armar lista de equipments
+
+#     serializer = EquipmentSerializer(equipments, many=True).data
+#     # deveolver lista de equipments
+
+#     return Response(serializer)
+
+
+# @api_view()
+# def equipment_retrieve(request, pk):
+#     ipdb.set_trace()
+#     import ipdb
+
+#     ipdb.set_trace()
+#     try:
+#         equipment = Equipment.objects.get(id=pk)
+#     except Equipment.DoesNotExist:
+#         return Response({"detail": "Equipment not found"}, status=404)
+#     # data = {
+#     #     "name": equipment.name,
+#     #     "description": equipment.description,
+#     # }
+#     serializer = EquipmentSerializer(equipment)
+
+#     return Response(serializer.data)
+
+
+# @api_view(["POST"])
+# def create_equipment(request):
+
+#     serializer = EquipmentSerializer(data=request.data)
+#     if serializer.is_valid():
+#         serializer.save()
+#         return Response(serializer.data)
+
+
+# @api_view(["PUT"])
+# def equipment_edition(request, pk):
+#     try:
+#         equipment = Equipment.objects.get(id=pk)
+#     except Equipment.DoesNotExist:
+#         return Response({"detail": "Equipment not found"}, status=404)
+#     serializer = EquipmentSerializer(equipment, data=request.data)
+#     if serializer.is_valid():
+#         serializer.save()
+#         return Response(serializer.data, status=status.HTTP_201_CREATED)
+#     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+# @api_view(["DELETE"])
+# def delete_equipment(request, pk):
+#     equipment = Equipment.objects.get(id=pk)
+#     equipment.delete()
+#     return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 # Create your views here.
